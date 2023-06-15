f7ae93d508a78da24499660f5f53d1f049913f63
hajsf <98168280+hajsf@users.noreply.github.com>
2023-06-15 14:54:31 +0300

    Create killtask.zig

---
+++ killtask.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+pub fn killtask(port: u16) !void {
+    // const port: u16 = 8181;
+    const allocator = std.heap.page_allocator;
+    var buf: [20]u8 = undefined;
+    var pid: []const u8 = undefined;
+    var kill_result: []const u8 = undefined;
+
+    if (builtin.os.tag == .macos or builtin.os.tag == .ios or builtin.os.tag == .linux) {
+        const cmd = [_][]const u8{ "lsof", "-ti", std.fmt.bufPrint(&buf, "tcp:{}", .{port}) catch unreachable };
+        const result = try std.ChildProcess.exec(.{
+            .allocator = allocator,
+            .argv = &cmd,
+        });
+        // Clean the tailing white spaces
+        pid = std.mem.trimRight(u8, result.stdout, &std.ascii.spaces);
+    } else if (builtin.os.tag == .windows) {
+        const cmd = [_][]const u8{ "cmd", "/C", std.fmt.bufPrint(&buf, "netstat -aon | findstr :{s}", .{port}) catch unreachable };
+        const result = try std.ChildProcess.exec(.{
+            .allocator = allocator,
+            .argv = &cmd,
+        });
+        // Clean the tailing white spaces
+        pid = std.mem.trimRight(u8, result.stdout, &std.ascii.spaces);
+    } else {
+        std.debug.print("Unsupported operating system\n", .{});
+        return;
+    }
+
+    std.log.info("The pid running at port {} is {s}, let's kill it.", .{ port, pid });
+
+    if (builtin.os.tag == .macos or builtin.os.tag == .ios or builtin.os.tag == .linux) {
+        const kill = [_][]const u8{ "kill", "-9", std.fmt.bufPrint(&buf, "{s}", .{pid}) catch unreachable };
+        const result = try std.ChildProcess.exec(.{
+            .allocator = allocator,
+            .argv = &kill,
+        });
+        kill_result = result.stderr;
+    } else if (builtin.os.tag == .windows) {
+        const kill = [_][]const u8{ "taskkill", "/F", "/PID", std.fmt.bufPrint(&buf, "{s}", .{pid}) catch unreachable };
+        const result = try std.ChildProcess.exec(.{
+            .allocator = allocator,
+            .argv = &kill,
+        });
+        kill_result = result.stderr;
+    }
+
+    if (kill_result.len == 0) {
+        std.log.info("killed confirmation.", .{});
+    } else {
+        std.log.err("{s}", .{kill_result});
+    }
+}
